f747f31b8761076ab8874fdc20391adb8234b70d
developerHub01 <developerhubytchannel@gmail.com>
2024-10-14 14:11:53 +0600

    create user api -- created

--- api/model/UserModel.php
+++ api/model/UserModel.php
@@ -5,6 +5,7 @@ require_once "../../config/database.php";
 class UserModel
 {
   private $conn;
+  private $table = "user";
 
   public function __construct()
   {
@@ -12,20 +13,37 @@ class UserModel
     $this->conn = $database->getConnection();
   }
 
-  // Fetch users from the database
-  public function fetchUsers()
+  public function findUserByEmail($email)
   {
-    $query = "SELECT * FROM users";
+    $query = "SELECT * FROM $this->table WHERE email = :email";
+
     $stmt = $this->conn->prepare($query);
-    $stmt->execute();
-    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    $stmt->execute([
+      ":email" => $email,
+    ]);
+
+    return $stmt->fetch(PDO::FETCH_ASSOC);
   }
 
-  // Add a user to the database
-  public function addUser($name, $email)
+  public function createUser($payload)
   {
-    $query = "INSERT INTO users (name, email) VALUES (:name, :email)";
+    if ($this->findUserByEmail($payload['email']))
+      return false;
+
+    $query = "INSERT INTO $this->table
+              (name, email, password)
+              VALUES 
+              (:name, :email, :password)";
+
     $stmt = $this->conn->prepare($query);
-    return $stmt->execute([':name' => $name, ':email' => $email]);
+
+    return $stmt->execute(
+      [
+        ":name" => $payload['name'],
+        ":email" => $payload['email'],
+        ":password" => $payload['password']
+      ]
+    );
   }
 }
--- api/v1/user/create.php
+++ api/v1/user/create.php
@@ -1,11 +1,46 @@
 <?php
+ini_set("display_errors", 1);
+
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 header("Access-Control-Allow-Methods: GET");
 
 include_once "../../utils/respons_builder.php";
+include "../../model/UserModel.php";
 
 if ($_SERVER['REQUEST_METHOD'] != "POST")
+  return response();
+
+$rawData = file_get_contents("php://input");
+$body = json_decode($rawData, true);
+
+if (!isset($body))
+  return response([
+    "statusCode" => 400,
+    "success" => false,
+    "message" => "Invalid JSON body"
+  ]);
+
+$payload = [];
+
+$payload['name'] = isset($body['name']) ? trim($body['name']) : null;
+$payload['email'] = isset($body['email']) ? trim($body['email']) : null;
+$payload['password'] = isset($body['password']) ? password_hash(trim($body['password']), PASSWORD_DEFAULT) : null;
+
+
+if (empty($payload['name']) || empty($payload['email']) || empty($payload['password']))
+  return response(
+    [
+      "statusCode" => 400,
+      "success" => false,
+      "message" => "Missing required fields",
+    ]
+  );
+
+$userModel = new UserModel();
+$isCreated = $userModel->createUser($payload);
+
+if (!$isCreated)
   return response();
 
 
