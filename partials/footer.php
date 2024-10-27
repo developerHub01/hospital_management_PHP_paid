e5f0a6081cd35adedfb30cca7aa82e26b745c9b0
developerHub01 <developerhubytchannel@gmail.com>
2024-10-28 04:46:27 +0600

    me endpoint complete

--- partials/footer.php
+++ partials/footer.php
@@ -2,6 +2,26 @@
 <script src="../public/js/bootstrap.js"></script>
 <script src="../public/js/jquery.min.js"></script>
 <script src="../public/js/index.js"></script>
+<script>
+  $(document).ready(() => {
+    $('#logout-btn').on('click', (e) => {
+      console.log("hello");
+
+      $.ajax({
+        url: '/api/v1/auth/logout.php',
+        type: 'GET',
+        success: (res) => {
+          if (res.success)
+            window.location.href = '/login.php';
+          else alert(res.message || "something went wrong")
+        },
+        error: (err) => {
+          alert(err.responseJSON.message || "something went wrong")
+        }
+      })
+    })
+  })
+</script>
 </body>
 
 </html>
